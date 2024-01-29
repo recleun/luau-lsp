08c3a2149b1792db0144df96b0872efec13fda3b
msix29 <abdulrehmanessam7567@gmail.com>
2024-01-29 07:21:57 +0300

    Update is-table-field.ts

--- server/src/utilities/type-checking/is-table-field.ts
+++ server/src/utilities/type-checking/is-table-field.ts
@@ -1,7 +1,15 @@
-import { TableField, TableFields } from "../../types";
+import { TableField, TableFieldType, TableFields } from "../../types";
 
 export function isTableField(obj: any): obj is TableField {
-	return typeof obj.Key !== "undefined" && typeof obj.Value !== "undefined" ;
+	return (
+		typeof obj.Key !== "undefined"
+		&& typeof obj.Type !== "undefined"
+		&& typeof obj.Value !== "undefined"
+	);
+}
+
+export function isTableFieldType(obj: any): obj is TableFieldType {
+	return typeof obj.Key !== "undefined" && typeof obj.Type !== "undefined";
 }
 
 export function isTableFields(obj: any): obj is TableFields {
