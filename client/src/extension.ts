e8ab49db253febfb19318b0d12cec2b79f83f600
msix29 <abdulrehmanessam7567@gmail.com>
2024-01-29 07:56:09 +0300

    Update extension.ts

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -4,7 +4,7 @@
  * ------------------------------------------------------------------------------------------ */
 
 import * as path from 'path';
-import { workspace, ExtensionContext, commands } from 'vscode';
+import { workspace, ExtensionContext } from 'vscode';
 
 import {
 	LanguageClient,
@@ -16,32 +16,27 @@ import {
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-	// The server is implemented in node
 	const serverModule = context.asAbsolutePath(
 		path.join('server', 'out', 'server.js')
 	);
+	const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
 
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
 	const serverOptions: ServerOptions = {
 		run: { module: serverModule, transport: TransportKind.ipc },
 		debug: {
 			module: serverModule,
 			transport: TransportKind.ipc,
+			options: debugOptions,
 		}
 	};
 
-	// Options to control the language client
 	const clientOptions: LanguageClientOptions = {
-		// Register the server for lua text documents
-		documentSelector: [{ scheme: 'file', language: 'lua' }],
+		documentSelector: [{ scheme: 'file', language: 'lua', pattern: "*.{lua,luau}" }],
 		synchronize: {
-			// Notify the server about file changes to '.clientrc files contained in the workspace
 			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
 		}
 	};
 
-	// Create the language client and start the client.
 	client = new LanguageClient(
 		'roblox-luau',
 		'Roblox Luau Language Server',
@@ -49,30 +44,13 @@ export function activate(context: ExtensionContext) {
 		clientOptions
 	);
 
-	// Start the client. This will also launch the server
-	client.start().then(() => {
-		// client.onNotification('$/command', (params) => {
-		// 	console.log(params);
-		// 	commands.executeCommand(params.command, params.data);
-		// });
-
-		// client.onNotification('$/move cursor', (params) => {
-		// 	params = JSON.parse(params);
-		// 	const newPosition = new vscode.Position(params.line, params.character);
-		// 	const editor = vscode.window.activeTextEditor;
-
-		// 	if (editor) {
-		// 		const selection = new vscode.Selection(newPosition, newPosition);
-		// 		editor.selection = selection;
-		// 		editor.revealRange(new vscode.Range(newPosition, newPosition));
-		// 	}
-		// });
-	});
+	client.start();
 }
 
 export function deactivate(): Thenable<void> | undefined {
 	if (!client) {
 		return undefined;
 	}
+	
 	return client.stop();
 }
